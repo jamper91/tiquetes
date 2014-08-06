8ea08bbb9a123b4127fa8b2b28c2e21f08eedf22
Alexander Correa Sanguino <chandy20@gmail.com>
2014-08-06 16:31:39 -0500

    para que registre los productos en la BD

---
+++ app/Test/Case/Controller/PeopleProductsControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+App::uses('PeopleProductsController', 'Controller');
+
+/**
+ * PeopleProductsController Test Case
+ *
+ */
+class PeopleProductsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.people_product',
+		'app.person_product',
+		'app.product',
+		'app.person',
+		'app.document_type',
+		'app.city',
+		'app.state',
+		'app.country',
+		'app.company',
+		'app.event',
+		'app.stage',
+		'app.location',
+		'app.paper',
+		'app.categoria',
+		'app.discount',
+		'app.gift',
+		'app.validation',
+		'app.entrada',
+		'app.input',
+		'app.input_state',
+		'app.events_registration_type',
+		'app.registration_type',
+		'app.shelf',
+		'app.delivery_method',
+		'app.delivery_methods_input',
+		'app.entradas_input',
+		'app.sale',
+		'app.inputs_sale',
+		'app.categorias_entrada',
+		'app.event_type',
+		'app.authorizations_user',
+		'app.user',
+		'app.type_user',
+		'app.department',
+		'app.log',
+		'app.authorization',
+		'app.form',
+		'app.personal_datum',
+		'app.forms_personal_datum',
+		'app.committee',
+		'app.committees_event',
+		'app.companies_event',
+		'app.hotel',
+		'app.events_hotel',
+		'app.payment',
+		'app.events_payment',
+		'app.data',
+		'app.committees_events_person'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('testIndex not implemented.');
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+		$this->markTestIncomplete('testView not implemented.');
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->markTestIncomplete('testAdd not implemented.');
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+		$this->markTestIncomplete('testEdit not implemented.');
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+		$this->markTestIncomplete('testDelete not implemented.');
+	}
+
+}
